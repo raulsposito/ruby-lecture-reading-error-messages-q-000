9d08f49097a20b41aee3aba9aa58454a9cd935b6
Raul Sposito <raulsposito@gmail.com>
2019-05-26 21:52:17 +0000

    Automatically backed up by Learn

--- lib/a_name_error.rb
+++ lib/a_name_error.rb
@@ -1,9 +1,3 @@
 "hello world"
-context 'name error' do
-  it 'raises a NameError when encountering undefined barewords' do
-expect{
-        load './lib/a_name_error.rb'
-      }.to_not raise_error
-    end
-  end
+
   
